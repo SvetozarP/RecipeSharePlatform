4831d7de30c030143b569b7ba92b7c9319489fa5
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-07-26 02:05:07 +0100

    Create superuser

---
+++ backend/core/management/commands/create_admin.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Create a superuser if none exist"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username=os.environ['ADMIN_USERNAME'],
+                email=os.environ['ADMIN_EMAIL'],
+                password=os.environ['ADMIN_PASSWORD']
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created'))
+        else:
+            self.stdout.write('Superuser already exists')
